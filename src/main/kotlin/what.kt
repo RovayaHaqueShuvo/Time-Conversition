572387715f90d1ae7e9479e13c7bdf00ff2eb544
RovayaHaqueShuvo <rovayahaqueshuvo@gmail.com>
2023-10-16 13:07:27 +0600

    select 1

--- src/main/kotlin/what.kt
+++ src/main/kotlin/what.kt
@@ -1,2 +1,35 @@
-class what {
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.Statement
+
+fun main() {
+    // Database connection information
+    val url = "jdbc:your_database_url_here"
+    val user = "your_username"
+    val password = "your_password"
+
+    try {
+        // Establish a database connection
+        val connection: Connection = DriverManager.getConnection(url, user, password)
+
+        // Create a SQL statement
+        val statement: Statement = connection.createStatement()
+
+        // Execute the SQL query to retrieve customer names from 'customers' table with state 'RS'
+        val query = "SELECT name FROM customers WHERE state = 'RS'"
+        val resultSet = statement.executeQuery(query)
+
+        // Iterate through the results and print the names
+        while (resultSet.next()) {
+            val name = resultSet.getString("name")
+            println(name)
+        }
+
+        // Close the resources
+        resultSet.close()
+        statement.close()
+        connection.close()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
--- src/test/kotlin/Time Converttion.kt
+++ src/test/kotlin/Time Converttion.kt
@@ -1,8 +1,15 @@
 import java.util.Scanner
 
-fun main(args: Array<String>) {
-
+fun main(args: Array<String>){
     var input = Scanner(System.`in`)
+    val A = input.nextInt()
+    val B = input.nextInt()
+    val C = input.nextInt()
+    val D = input.nextInt()
 
-    var N = input.nextInt()
+    if (B > C && D > A && (C + D) > (A + B) && C >= 0 && D >= 0 && A % 2 == 0) {
+        println("Valores aceitos")
+    } else {
+        println("Valores nao aceitos")
+    }
 }
